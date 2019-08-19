ce2c89b4cef89d9726d5ebba85b5dd1d019279a9
LeDon-SP <1021752017@qq.com>
2019-08-19 22:53:26 +0800

    add 问题中心，导航栏分离

--- src/main/java/life/ledon/community/controller/indexController.java
+++ src/main/java/life/ledon/community/controller/indexController.java
@@ -30,7 +30,7 @@ public class IndexController {
     public String index(HttpServletRequest request,
                         Model model,
                         @RequestParam(name = "page", defaultValue = "1") Integer page,
-                        @RequestParam(name = "size", defaultValue = "2") Integer size){
+                        @RequestParam(name = "size", defaultValue = "8") Integer size){
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
