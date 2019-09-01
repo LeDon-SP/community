6796e05fbb8502ce9e342961a9dae66ef70b7f23
LeDon-SP <1021752017@qq.com>
2019-09-01 23:05:49 +0800

    增加规范标签库

---
+++ src/main/java/life/ledon/community/cache/Tagche.java
@@ -0,0 +1,53 @@
+package life.ledon.community.cache;
+
+import life.ledon.community.dto.TagDTO;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Tagche {
+    public static List<TagDTO> get(){
+        List<TagDTO> tagDTOS = new ArrayList<>();
+        TagDTO program = new TagDTO();
+        program.setCategoryName("开发语言");
+        program.setTags(Arrays.asList("js","php","html","css","node","python"));
+        tagDTOS.add(program);
+
+        TagDTO framework = new TagDTO();
+        framework.setCategoryName("平台框架");
+        framework.setTags(Arrays.asList("Spring","struts","ruby-on-rails","koa","express","django","flask"));
+        tagDTOS.add(framework);
+
+        TagDTO server = new TagDTO();
+        server.setCategoryName("服务器");
+        server.setTags(Arrays.asList("docker","unix","linux","apache","centos","hadoop","nginx"));
+        tagDTOS.add(server);
+
+        TagDTO database = new TagDTO();
+        database.setCategoryName("数据库");
+        database.setTags(Arrays.asList("mysql","oracle","mongodb","nosql","sqlserver","redis","sql"));
+        tagDTOS.add(database);
+
+        TagDTO tool = new TagDTO();
+        tool.setCategoryName("开发工具");
+        tool.setTags(Arrays.asList("git","svn","maven","IDEA","eclipse","github","vim"));
+        tagDTOS.add(tool);
+
+        return tagDTOS;
+    }
+
+    public static String filterInvalid(String tags){
+        String[] split = StringUtils.split(tags,",");
+        List<TagDTO> tagDTOS = get();
+        List<String> tagList = tagDTOS.stream().flatMap(tag -> tag.getTags().stream()).collect(Collectors.toList());
+
+        String invalid = Arrays.stream(split).filter(t -> !tagList.contains(t)).collect(Collectors.joining(","));
+
+        return invalid;
+    }
+}
--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -140,3 +140,20 @@ function collapseComments(e) {
         e.classList.add("active");
     }*/
 }
+
+function showSelectTag() {
+    $("#select-tag").show();
+}
+
+function selectTag(e) {
+    var value = e.getAttribute("data-tag");
+    var previous = $("#tag").val();
+
+    if (previous.indexOf(value) == -1) {
+        if (previous) {
+            $("#tag").val(previous + ',' + value);
+        } else {
+            $("#tag").val(value);
+        }
+    }
+}
