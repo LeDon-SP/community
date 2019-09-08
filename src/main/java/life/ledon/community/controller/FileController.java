f727ef634db7250a73aee9468a52ee40e9c169e5
LeDon-SP <1021752017@qq.com>
2019-09-08 23:41:21 +0800

    增加上传图片到云服务器

--- src/main/java/life/ledon/community/controller/FileController.java
+++ src/main/java/life/ledon/community/controller/FileController.java
@@ -1,15 +1,34 @@
 package life.ledon.community.controller;
 
 import life.ledon.community.dto.FileDTO;
+import life.ledon.community.provider.UCloudProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @Controller
 public class FileController {
+
+    @Autowired
+    private UCloudProvider uCloudProvider;
+
     @RequestMapping("/file/upload")
     @ResponseBody
-    public FileDTO upload(){
+    public FileDTO upload(HttpServletRequest request){
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        MultipartFile file = multipartRequest.getFile("editormd-image-file");
+
+        try {
+            uCloudProvider.upload(file.getInputStream(),file.getContentType(),file.getOriginalFilename());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         FileDTO fileDTO = new FileDTO();
         fileDTO.setSuccess(1);
         fileDTO.setUrl("/images/a.jpg");
