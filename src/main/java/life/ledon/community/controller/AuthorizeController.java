57f6110c6393eeb5609d3cf8ac606fa4d10c94f0
LeDon-SP <1021752017@qq.com>
2019-10-14 14:27:31 +0800

    修改回调地址

--- src/main/java/life/ledon/community/controller/AuthorizeController.java
+++ src/main/java/life/ledon/community/controller/AuthorizeController.java
@@ -6,6 +6,7 @@ import life.ledon.community.mapper.UserMapper;
 import life.ledon.community.model.User;
 import life.ledon.community.provider.GitHubProvider;
 import life.ledon.community.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -18,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
+@Slf4j
 public class AuthorizeController {
     @Autowired
     private GitHubProvider gitHubProvider;
@@ -60,6 +62,7 @@ public class AuthorizeController {
             response.addCookie(new Cookie("token",token));
             return "redirect:/";
         } else {
+            log.error("callback get github error,{}",gitHubUser);
             //登录失败，重新登录
             return "redirect:/";
         }
